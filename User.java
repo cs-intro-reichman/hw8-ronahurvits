2de8829c333a0b7522701f81b4b966e1a22bac4a
Rona Hurvits <ronahurvits@Ronas-MacBook-Pro.local>
2025-01-09 15:04:07 +0200

    hw draft num 2 - user class

--- User.java
+++ User.java
@@ -51,8 +51,9 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (this.fCount < maxfCount) {
+        if ((this.fCount < maxfCount) && (!this.follows(name))) {
             this.follows[fCount] = name;
+            fCount++;
         }
         return false;
     }
